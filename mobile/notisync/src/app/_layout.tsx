b758ef1fc69301d4ca8d6c59040649af8901aa67
Faithfulisuh <faithfulisuh15@gmail.com>
2025-10-06 21:33:26 +0100

    fixed

--- mobile/notisync/src/app/_layout.tsx
+++ mobile/notisync/src/app/_layout.tsx
@@ -1,10 +1,46 @@
-import "../global.css";
-import { Stack } from "expo-router";
+import '../global.css';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { Suspense, useEffect } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export default function Layout() {
+// RootLayout component serves as the main layout for the application
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Light': require('../assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }}></Stack.Screen> 
-    </Stack>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Suspense
+          fallback={
+            <View className="flex-1 items-center justify-center">
+              <ActivityIndicator size={'large'} />
+              <Text>Loading...</Text>
+            </View>
+          }>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+          </Stack>
+        </Suspense>
+    </GestureHandlerRootView>
   );
-}
+};
+
+export default RootLayout;
--- mobile/notisync/tailwind.config.js
+++ mobile/notisync/tailwind.config.js
@@ -2,11 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
   future: {
     hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+        myblue: '#3A5BFF', //blue
+        myred: '#FF3E41', //red
+        mygray: '#DDDDDD', //gray
+        myblack: '#161622', //custom black
+        myorange: '#FF9C01', //orange
+      },
+      fontFamily: {
+        pthin: ['Poppins-Thin', 'sans-serif'],
+        pextralight: ['Poppins-ExtraLight', 'sans-serif'],
+        plight: ['Poppins-Light', 'sans-serif'],
+        pregular: ['Poppins-Regular', 'sans-serif'],
+        pmedium: ['Poppins-Medium', 'sans-serif'],
+        psemibold: ['Poppins-SemiBold', 'sans-serif'],
+        pbold: ['Poppins-Bold', 'sans-serif'],
+        pextrabold: ['Poppins-ExtraBold', 'sans-serif'],
+        pblack: ['Poppins-Black', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
